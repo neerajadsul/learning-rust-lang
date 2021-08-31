9de72560a88be730823d91d49fc0832ae1076d12
tantradnya <neeraj.adsul@gmail.com>
2021-08-31 17:46:07 +0100

    Implements hello Rust in three  unicode languages

--- hellounicode/src/main.rs
+++ hellounicode/src/main.rs
@@ -1,3 +1,6 @@
 fn main() {
-    println!("Hello, world!");
+    println!("English: Hello, world!");
+    println!("Marathi: नमस्कार Rust");
+    println!("Japanes: こんにちは Rust");
+    println!("Chinese: 你好 Rust");
 }
