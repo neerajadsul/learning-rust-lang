d156271c72bafc53f0942d9ad5f9631d47ecab67
tantradnya <neeraj.adsul@gmail.com>
2021-09-25 18:21:27 +0100

    adds error handler with message for invalid input

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -16,7 +16,13 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-        let guess: u32 = guess.trim().parse().expect("Please enter a valid number");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Please enter a valid number");
+                continue;
+            }
+        };
 
         println!("You guessed: {}", guess);
 
