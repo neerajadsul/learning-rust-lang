79f05c41dc927f348f81c82b03a0ef306b944bdc
tantradnya <neeraj.adsul@gmail.com>
2021-12-28 12:35:11 +0000

    implements gcd calculation request

--- gcd-web-app/src/main.rs
+++ gcd-web-app/src/main.rs
@@ -1,13 +1,20 @@
-use std::str::FromStr;
 use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    m: u64,
+    n: u64,
+}
 
 fn main() {
     let port_number = "5555";
     let host_addr = "localhost";
 
     let server = HttpServer::new(|| {
-        App::new().
-        route("/", web::get().to(get_index))
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
     });
 
     println!("Running web-server on http://localhost:{}", port_number);
@@ -31,6 +38,21 @@ fn get_index() -> HttpResponse {
         )
 }
 
+fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+    if form.m == 0 || form.n == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("GCD of zero is meaningless");
+    }
+
+    let response = format!(
+        "The GCD of numbers {} and {} is <b>{}</b>",
+        form.m, form.n, gcd(form.m, form.n));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
     assert!( n != 0 && m != 0);
