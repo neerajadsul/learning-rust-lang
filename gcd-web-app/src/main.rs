348919a2f60219349bf1aab384c8bc4a4c1e60a9
tantradnya <neeraj.adsul@gmail.com>
2021-12-28 10:58:48 +0000

    adds command line argument processing

--- gcd-web-app/src/main.rs
+++ gcd-web-app/src/main.rs
@@ -1,7 +1,29 @@
+use std::str::FromStr;
+use std::env;
+
 fn main() {
     let m = 108;
     let n = 210;
     println!("GCD of {} and {} is {}", m, n, gcd(m, n));
+
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg)
+        .expect("error parsing arguments"));
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = gcd(d, *m);
+    }
+
+    println!("GCD of {:?} is {}", numbers, d);
 }
 
 fn gcd(mut n: u64, mut m: u64) -> u64 {
