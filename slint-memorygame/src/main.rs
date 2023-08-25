4cfda5be338506c9ef6ee2ee4c788098a9bbc04b
neerajadsul <neeraj.adsul@gmail.com>
2023-08-25 13:59:07 +0100

    Simple text message in a Window.

--- slint-memorygame/src/main.rs
+++ slint-memorygame/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+
+slint::slint! {
+    export component MainWindow inherits Window {
+        height: 200px;
+        width: 300px;
+        Text {
+            text: "Hello Slint UI!";
+            color: blue;
+        }
+     }
 }
+
+fn main() {
+    MainWindow::new().unwrap().run().unwrap();
+}
+
