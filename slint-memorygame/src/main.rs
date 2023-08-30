1c04b501f2c2695827e7b6f59ec8192067509a56
neerajadsul <neeraj.adsul@gmail.com>
2023-08-30 19:54:30 +0100

    Game logic to hide unmatched pair after 1 sec, uncover matched pairs.

--- slint-memorygame/src/main.rs
+++ slint-memorygame/src/main.rs
@@ -56,6 +56,9 @@ slint::slint! {
         width: 374px;
         height: 374px;
 
+        callback check_if_pair_solved();
+        in property <bool> disable_tiles;
+
         in property <[TileData]> memory_tiles: [
             {image: @image-url("icons/bicycle.png")},
             {image: @image-url("icons/at.png")},
@@ -76,7 +79,10 @@ slint::slint! {
             open-curtain: tile.image-visible || tile.solved;
             solved: tile.solved;
             clicked => {
-                tile.image-visible = !tile.image-visible;
+                if (!root.disable-tiles) {
+                    tile.image-visible = !tile.image-visible;
+                    root.check_if_pair_solved();
+                }
             }
         }
      }
@@ -98,7 +104,42 @@ fn main() {
     tiles.shuffle(&mut rng);
 
     let tiles_model = std::rc::Rc::new(slint::VecModel::from(tiles));
-    main_window.set_memory_tiles(tiles_model.into());
+    main_window.set_memory_tiles(tiles_model.clone().into());
+
+    let main_window_weak = main_window.as_weak();
+
+    main_window.on_check_if_pair_solved(move || {
+        let mut flipped_tiles = 
+            tiles_model.iter().enumerate().filter(|(_, tile)| tile.image_visible && !tile.solved);
+
+        if let (Some((t1_idx, mut t1)), Some((t2_idx, mut t2))) =
+            (flipped_tiles.next(), flipped_tiles.next())
+            {
+                let is_pair_solved = t1 == t2;
+                if is_pair_solved {
+                    t1.solved = true;
+                    tiles_model.set_row_data(t1_idx, t1);
+                    t2.solved = true;
+                    tiles_model.set_row_data(t2_idx, t2);
+                } else {
+                    let main_window = main_window_weak.unwrap();
+                    main_window.set_disable_tiles(true);
+                    let tiles_model = tiles_model.clone();
+
+                    slint::Timer::single_shot(std::time::Duration::from_secs(1), move || {
+                        main_window.set_disable_tiles(false);
+                        t1.image_visible = false;
+                        tiles_model.set_row_data(t1_idx, t1);
+                        t2.image_visible = false;
+                        tiles_model.set_row_data(t2_idx, t2);
+                    });
+                }
+            }
+
+    });
+
+
+
 
     main_window.run().unwrap();
 }
