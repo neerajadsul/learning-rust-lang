8b7f31730611abcdae49d03f295046d02b4e314b
neerajadsul <neeraj.adsul@gmail.com>
2024-03-24 13:44:52 +0000

    Ability to accept defeat.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -5,16 +5,21 @@ use std::cmp::Ordering;
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let secret_number = rand::thread_rng().gen_range(1..=1000);
     // println!("Secret Number is: {}", secret_number);
     let mut attempts: u32 = 0;
     loop {
-        println!("Please input your guess: ");
+        println!("Please input your guess: (enter q to cede)");
         let mut guess = String::new();
 
         io::stdin()
         .read_line(&mut guess)
         .expect("Failed to read line");
+
+        if guess.trim().eq_ignore_ascii_case("q") {
+            println!("You lose, number was: {}", secret_number);
+            break;
+        }
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
